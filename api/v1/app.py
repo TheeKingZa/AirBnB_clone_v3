6154ed056f0ad50331fe50dde5eb57634477ba14
Simbonge Ngqentsu <ngqentsu0307@gmail.com>
2024-01-08 06:28:18 -0800

    New view for Review object that handles all default RESTFul API actions

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,31 +7,40 @@ from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 
-app = Flask(__name__)  # Create a Flask web application instance
-CORS(app, resources={"/api/*": {"origins": "0.0.0.0"}})  # Enable CORS for the entire API
-app.url_map.strict_slashes = False  # Disable strict slashes at the end of routes
-app.register_blueprint(app_views)  # Register the blueprint containing API routes
+# Create a Flask web application instance
+app = Flask(__name__)
+# Enable CORS for the entire API
+CORS(app, resources={"/api/*": {"origins": "0.0.0.0"}})
+# Disable strict slashes at the end of routes
+app.url_map.strict_slashes = False
+# Register the blueprint containing API routes
+app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def tear_down(self):
-    """ Removes the current SQLAlchemy session when the app context is torn down"""
+    """Removes the current SQLAlchemy session for tear down"""
     storage.close()
+
 
 @app.errorhandler(404)
 def not_found(error):
-    """ Handles 404 errors by returning a JSON response with an 'error' message"""
+    """Handles 404 error with 'Not Found' error message"""
     return make_response(jsonify({"error": "Not found"}), 404)
+
 
 @app.errorhandler(400)
 def bad_request(error):
-    """ Handles 400 errors by returning a JSON response with an 'error' message"""
+    """ Handles 400 errors with an 'error' message"""
     return make_response(jsonify({'error': error.description}), 400)
+
 
 if __name__ == "__main__":
     # Check if the script is being run directly
     if os.getenv("HBNB_API_HOST") and os.getenv("HBNB_API_PORT"):
         # If environment variables for host and port are set, use them
-        app.run(host=os.getenv("HBNB_API_HOST"), port=os.getenv("HBNB_API_PORT"), threaded=True)
+        app.run(host=os.getenv("HBNB_API_HOST"),
+                port=os.getenv("HBNB_API_PORT"), threaded=True)
     else:
         # Otherwise, run the app on host '0.0.0.0' and port 5000
         app.run(host='0.0.0.0', port=5000, threaded=True)
